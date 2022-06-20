ed2f27cf7d154ffdcca7a4ffe9af2847921f56a1
Ioannis Maras <maras2002@gmail.com>
2022-06-20 08:21:09 +0300

    Version 0.01

---
+++ lib/DemoApp.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'dart:ui';
+
+class DemoApp extends StatefulWidget {
+  @override
+  _DemoAppState createState() => _DemoAppState();
+}
+
+class _DemoAppState extends State<DemoApp> {
+  DateTime? selectedDay;
+  List<CleanCalendarEvent>? selectedEvent;
+
+  final Map<DateTime, List<CleanCalendarEvent>> events = {
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day): [
+      CleanCalendarEvent('Event A',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 12, 0),
+          description: 'A special event',
+          color: Colors.blue),
+    ],
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 2):
+        [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+      CleanCalendarEvent('Event Δ',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 11, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+    ],
+  };
+
+  void _handleData(date) {
+    setState(() {
+      selectedDay = date;
+      selectedEvent = events[selectedDay] ?? [];
+    });
+    //print(selectedDay);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    selectedEvent = events[selectedDay] ?? [];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Calendar(
+            startOnMonday: true,
+            selectedColor: Colors.blue,
+            todayColor: Colors.green,
+            eventColor: Colors.pink,
+            eventDoneColor: Colors.amber,
+            bottomBarColor: Colors.deepOrange,
+            onRangeSelected: (range) {
+              print('selected Day ${range.from},${range.to}');
+            },
+            onDateSelected: (date) {
+              return _handleData(date);
+            },
+            events: events,
+            isExpanded: true,
+            dayOfWeekStyle: const TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+              //fontWeight: FontWeight.w100,
+            ),
+            bottomBarTextStyle: const TextStyle(
+              color: Colors.white,
+            ),
+            hideBottomBar: false,
+            hideArrows: false,
+            weekDays: ['Δευ', 'Τρί', 'Τετ', 'Πεμ', 'Παρ', 'Σαβ', 'Κυρ'],
+          ),
+        ),
+      ),
+    );
+  }
+}
