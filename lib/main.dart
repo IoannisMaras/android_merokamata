4ca4594d4ea8f752f0a3fb5b3232abfbf346118e
Ioannis Maras <maras2002@gmail.com>
2022-06-23 12:40:19 +0300

    half way implementing Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
 
 import 'DemoApp.dart';
 
-void main() {
+late Box box
+
+void main() async {
+  var path = Directory.current.path;
+  Hive.init(path);
+  box = await Hive.openBox('EventsBox');
+  Hive.registerAdapter(MyEventsAdapter());
   runApp(
     // Adding ProviderScope enables Riverpod for the entire project
     ProviderScope(child: MyApp()),
